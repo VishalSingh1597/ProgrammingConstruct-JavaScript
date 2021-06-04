aed2e346682638aa4166318c9b809c33cb3e3e45
VishalSingh1597 <80283079+VishalSingh1597@users.noreply.github.com>
2021-06-04 18:46:50 +0530

    [Vishal]Added:Calculating Area into Acre

---
+++ Sequence/AreaInAcre.js
@@ -0,0 +1,9 @@
+var plot = (60*40) * 25;
+console.log("Area of 25 plot of (60*40) in square feet is : "+plot);
+
+var sqmeter = plot/10.764;
+console.log("Area of 25 plot of (60*40) in square meter is : "+sqmeter);
+
+var acre = sqmeter*0.00024711;
+console.log("Area of 25 such plot in acre is : "+acre);
+
